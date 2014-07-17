c50da9cbda5ac70b69c4e755ba24e6c7800ffa10
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-17 17:00:10 -0300

    Initial Commit

---
+++ EjemploScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EjemploScroll
+//
+//  Created by Leandro Fantin on 17/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
